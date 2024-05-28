bbdb0a8d179b68113d33ae610c747f447b9330cb
Craig Andrews <candrews@integralblue.com>
2024-05-28 09:04:55 -0400

    When running frontend only, proxy /actuator
    When using `npm start` to run the frontend only, have vite proxy /actuator/*

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      "^/(api)": {
+      "^/(api|actuator)/": {
         target: "https://localhost:8443",
       },
     },
