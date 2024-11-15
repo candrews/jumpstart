4b498460c839558722be418b90e88198f22d45f5
Craig Andrews <candrews@integralblue.com>
2024-11-15 10:15:39 -0500

    JumpstartApplicationTests: remove unnecessary import
    Same package classes are always implicitly imported.

--- src/test/java/com/integralblue/demo/jumpstart/JumpstartApplicationTests.java
+++ src/test/java/com/integralblue/demo/jumpstart/JumpstartApplicationTests.java
@@ -6,8 +6,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import com.integralblue.demo.jumpstart.JumpstartApplication;
-
 @SpringBootTest
 /* default */ class JumpstartApplicationTests {
 	@Autowired
